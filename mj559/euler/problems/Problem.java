2614b582c65821dffdd6072fba7f7211987f8946
Marzipan <mattjones90@gmail.com>
2012-09-03 00:03:12 +0100

    Solutions to problems 1 + 2

---
+++ mj559/euler/problems/Problem.java
@@ -0,0 +1,6 @@
+package mj559.euler.problems;
+
+public interface Problem {
+	// Solves the problem!
+	public void solve();
+}
---
+++ mj559/euler/problems/Problem1.java
@@ -0,0 +1,37 @@
+package mj559.euler.problems;
+
+import mj559.euler.util.Timer;
+
+/**
+ * Problem 1:
+ * 
+ * If we list all the natural numbers below 10 that are multiples 
+ * of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is
+ * 23. Find the sum of all the multiples of 3 or 5 below 1000.
+ * 
+ */
+
+public class Problem1 implements Problem {
+
+	Timer t = new Timer();
+	
+	@Override
+	public void solve() {
+		
+		t.start();
+		int total = 0;
+		for(int i = 0; i < 1000; i++){
+			if (i % 5 == 0 || i % 3 == 0){
+				total += i;
+			}
+		}
+		long time = t.stop();
+		System.out.println("The solution is: " +  total);
+		System.out.println("Took " + time + "ms.");
+	}
+
+	public static void main(String[] args) {
+		Problem p = new Problem1();
+		p.solve();
+	}
+}
